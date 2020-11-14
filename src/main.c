5314b44230353432bcb714afcffd0e9bf4d2c71e
Andrew Elsayeh <andrew.elsayeh@tum.de>
2020-11-14 16:45:10 +0100

    Solved Coding Challenge 2

--- src/main.c
+++ src/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <espl_lib.h>
+#include <string.h>
 
 int main() {
-	printf("Hello ESPL\n");
-	printf("This is a new print\n");
+
+	unsigned int num; 
+	char 	     repeat_program;
+	while(1)
+	{
+		printf("Enter a number:");
+		scanf("%d", &num);
+
+		char* word = num_to_words(num);
+		printf("Your number is spelled %s\n", word);
+	
+		
+		printf("Would you like to continue? [y/n]: ");
+		scanf(" %c", &repeat_program);
+
+		if(repeat_program == 'n')
+		{
+		break;
+		}
+	}
+
 	return 0;
 }
 
